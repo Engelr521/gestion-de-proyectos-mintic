4be45449beebe115419c7de76566b07685d1364d
Leonardo Ramirez <angelr521@hotmail.com>
2021-11-05 15:38:51 -0500

    Agregar Tailwind y rutas de muestra

---
+++ pages/leonardo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const leonardo = () => {
+    return (
+        <div>
+            esta es la pagina leonardo
+        </div>
+    )
+};
+
+export default leonardo;
---
+++ pages/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const test = () => {
+    return (
+        <div>
+            soy la pagina de test
+        </div>
+    )
+}
+
+export default test
