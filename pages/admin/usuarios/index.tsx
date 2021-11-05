1abc105cb1b501de1ddb6cb4c7f8a6581a56e314
Leonardo Ramirez <angelr521@hotmail.com>
2021-11-05 17:44:26 -0500

    Agregar titulos en paginas y archivo _document.tsx para poder agregar script de fontawesome

--- pages/admin/usuarios/index.tsx
+++ pages/admin/usuarios/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
+
 
 const index = () => {
     return (
         <div>
+            <Head>
+                <title>Admin Usuarios | Gestion De Proyectos</title>
+            </Head>
            <div>pagina de admin de usuarios</div>
            <Link href='/'>
             <a className ="cursor-pointer">Ir al la Pagina de Home</a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import Head from 'next/head'
 
 
 const Home: NextPage = () => {
   return (
     <div className="bg-green-300">
+      <Head>
+        <title>Home | Gestion De Proyectos</title>
+      </Head>
       <p>pagina de index </p>
       <Link href="/admin/usuarios"> 
         <a className="cursor-pointer">Ir a Admin usuarios</a> 
       </Link>
+      <div>
+      <i className="fas fa-home"></i>
+      </div>
     </div>
   )
 }
